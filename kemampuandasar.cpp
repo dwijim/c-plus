2c1989e27451546ef7738ff596034fc66d2a9e51
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-08-28 17:14:28 +0700

    Add files via upload

--- kemampuandasar.cpp
+++ kemampuandasar.cpp
@@ -16,7 +16,7 @@
 
 using namespace std;
 
-// mendefinisikan pilihan ESC nilai 27
+// mendefinisikan tombol ESC nilai 27
 #define KEY_ESC 27
 
 // kolom tengah layar
@@ -29,7 +29,7 @@ using namespace std;
 int main(){
 
    // deklarasi variabel
-   int pilihan;
+   int tombol;
    short int baris, kolom;
    
    // memberi posisi baris di bagian bawah
@@ -41,11 +41,11 @@ int main(){
   // menginisiasi layar untuk penggunaan ncurses
    initscr();
 
-   // supaya dapat dideteksi penekanan pilihan khusus seperti
+   // supaya dapat dideteksi penekanan tombol khusus seperti
    // F1 .. F10, panah kanan, panah kiri dst ...
    keypad(stdscr,TRUE);
 
-   // menyembunyikan atau menampilan kursor
+   // menyembunyikan tampilan kursor
    curs_set(1);
 
   // nodelay - enable or disable block during read
@@ -53,43 +53,58 @@ int main(){
    
    // tampilan awal di tengah layar
    mvprintw(baris,kolom,"Kemampuan Dasar Komputer");
-
-   pilihan = 1;
-   // membuat looping selama tidak menekan pilihan ESC
-         /*
-     while (pilihan != 1) {
-         
-                // cetak sesuatu sesuai dengan baris dan kolom
-                // di kanan dan kiri diberi spasi
-                // untuk menghapus
                 mvprintw(baris+2,kolom,"1. Menjumlahkan bilangan");
                 mvprintw(baris+3,kolom,"2. Mengalikan bilangan");
                 mvprintw(baris+4,kolom,"3. Membandingkan bilangan");
                 mvprintw(baris+5,kolom,"4. Melakukan perulangan");
-                cin >> pilihan;
+                mvprintw(baris+7,kolom,"5. Melakukan perulangan");
+                mvprintw(baris+9,kolom,"Pilihan Anda: ");
+
+   // membuat looping selama tidak menekan tombol ESC
+     while (tombol != KEY_ESC) {
+          if ((tombol = getch()) == ERR) {
+              /* user hasn't responded
+               ...
+              */
+          }
+          else {
+              /* user has pressed a key ch
+               ...
+              */ 
+             if(tombol == KEY_ENTER)
+                { 
+                  // misal kalau menekan tombol enter
+                  // ada sesuatu yang akan dilakukan
+                  // dapat dituliskan di sini
+                  }  
+             else if(tombol == KEY_LEFT)
+                { 
+                  // bergerak ke kiri, jadi kolom berkurang
+                  kolom = kolom - 1;
+
+                  // jika sudah mentok ke kiri, kolom tetap 1
+                  if (kolom==1) kolom=1;
+                }  
+             else if(tombol == KEY_RIGHT)
+                { 
+                  // bergerak ke kanan, jadi kolom bertambah
+                  kolom = kolom + 1;
+
+                  // jika sudah mentok ke kanan, kolom tetap 75
+                  if (kolom==75) kolom=75;
+                }  
+
+                // cetak sesuatu sesuai dengan baris dan kolom
+                // di kanan dan kiri diberi spasi
+                // untuk menghapus
+                mvprintw(baris,kolom," aku ");
+                
+	  }  
      }
-                */
 
    // menutup layar mode ncurses
-    printw("Hello World !!!");
-   getch();
    endwin();  
 
    // kembali ke program utama
    return 0;
 }
-
-
-/* ncurses1.c */
-/*
- * 
-#include <ncurses.h>
-int main()
-{
-    initscr();
-    printw("Hello World !!!");
-    getch();
-    endwin();
-    return 0;
-}
-*/
