7d78d068f440426799c5ef208ec5ed5115c8b5bd
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-09-13 17:14:09 +0700

    Update luasegitiga.cpp

--- ncurses/luasegitiga.cpp
+++ ncurses/luasegitiga.cpp
@@ -1,12 +1,12 @@
 /* -------------------------------
-   contoh format dasar program C++
+   contoh program C++
    dengan menggunakan pustaka ncurses
-   nama file: formatdasar.cpp
+   nama file: luassegitiga.cpp
    dicompile dengan perintah:
-   g++ formatdasar.cpp -o format -l ncurses
+   g++ luassegitiga.cpp -o luas -l ncurses
    
    program ini ditulis di cocalc.com
-   pada tanggal 12 september 2021
+   pada tanggal 13 september 2021
 
    ------------------------------- */
 
@@ -34,16 +34,21 @@ int main(){
   // menampilkan tulisan di layar pada posisi 5,10
   mvprintw(5,10,"Menghitung Luas");
   
-  // memasukkan suatu nilai
+  // memasukkan nilai alas dalam format pecahan
+  // misal 7.0
   mvprintw(7,10,"Masukan alas: ");
   scanw("%f",&alas);
 
+  // memasukkan nilai tinggi dalam format pecahan
+  // misal 2.0
   mvprintw(8,10,"Masukan tinggi: ");
   scanw("%f",&tinggi);
   
-  
+  // menghitung luas segi tiga
   luas = (alas*tinggi)/2;
   
+  // menampilkan nilai luas
+  // karena tipe pecahan, jadi pakai %f
   mvprintw(9,10,"Luas : %f",luas);
   
   // menahan tampilan layar
@@ -55,10 +60,3 @@ int main(){
   // hasil fungsi main() bertipe integer
   return 11;
 }
-
-
-
-
-
-
-
