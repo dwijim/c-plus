9d49f4322174ff1bee0b617e21e9e0067d18f250
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-09-13 10:08:19 +0700

    Update kotak.cpp

--- ncurses/kotak.cpp
+++ ncurses/kotak.cpp
@@ -1,9 +1,9 @@
 /* -------------------------------
    contoh format dasar program C++
    dengan menggunakan pustaka ncurses
-   nama file: formatdasar.cpp
+   nama file: kotak.cpp
    dicompile dengan perintah:
-   g++ formatdasar.cpp -o format -l ncurses
+   g++ kotak.cpp -o kotak -l ncurses
    
    program ini ditulis di cocalc.com
    pada tanggal 12 september 2021
@@ -23,26 +23,75 @@ using namespace std;
 #define pojok (43)
 //#     pojok  "+"
 
+#define horisontal (45)
+//#     garis mendatar  "-"
+
+#define vertikal (124)
+//#     garis vertikal  "|"
+
+void kotak(int baris11, int kolom11,int baris22,  int kolom22)
+{
+  int kolom1,kolom2,baris1,baris2;
+  int kol,bar;
+  // baris atas mendatar
+  kolom1 = kolom11;
+  baris1 = baris11;
+  kolom2 = kolom22;
+  baris2 = baris22;
+  
+  mvprintw(baris1,kolom1,"%c",pojok);
+  
+  for (kol=kolom1+1;kol<=kolom2-1;kol++)
+  {    
+    mvprintw(baris1,kol,"%c",horisontal);    
+  }
+  mvprintw(baris1,kolom2,"%c",pojok);
+
+  // baris samping kanan ke bawah
+  
+  for (bar=baris1+1;bar<=baris2-1;bar++)
+  {    
+    mvprintw(bar,kolom2,"%c",vertikal);    
+  }
+  mvprintw(baris2,kolom2,"%c",pojok);
+ 
+  // baris bawah ke kiri
+  
+  for (kol=kolom2-1;kol>=kolom1+1;kol--)
+  {    
+    mvprintw(baris2,kol,"%c",horisontal);    
+  }
+  mvprintw(baris2,kolom1,"%c",pojok);
+
+  // baris samping kiri ke atas
+  
+  for (bar=baris2-1;bar>=baris1+1;bar--)
+  {    
+    mvprintw(bar,kolom1,"%c",vertikal);    
+  }
+
+  
+  
+}
+
 int main(){
   
-  int bilangan;
-  
+ 
   //mengawali layar untuk ncurses
   initscr();
   
   // membersihkan layar
   clear();
   
-  // menampilkan tulisan di layar pada posisi 5,10
-  mvprintw(5,10,"Jurusan Ilmu Komputer");
-  
-  // memasukkan suatu nilai
-  mvprintw(7,10,"Membuat kotak");
-  mvprintw(9,10,"Kiri atas %d %c ...",pojok,pojok);
+  kotak(3,8,9,15);
+
+  kotak(11,20,20,45);
+
+  kotak(3,30,10,50);
   
   // menahan tampilan layar
   getch();
-  
+   
   //mengakhiri layar ncurses
   endwin();
   
