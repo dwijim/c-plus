47ab9177a0029a98cbbfb0aa3ad1d6fa95e0e8fe
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-10-02 11:56:25 +0700

    Update array.cpp

--- ncurses/array.cpp
+++ ncurses/array.cpp
@@ -20,3 +20,59 @@ int main()
 
   return 1;
 }
+
+/* -------------------------------
+   contoh format dasar program C++
+   dengan menggunakan pustaka ncurses
+   nama file: formatdasar.cpp
+   dicompile dengan perintah:
+   g++ formatdasar.cpp -o format -l ncurses
+   
+   program ini ditulis di cocalc.com
+   pada tanggal 12 september 2021
+
+   ------------------------------- */
+
+// ini karena menggunakan pustaka ncurses
+#include "ncurses.h"
+
+// input/output di dalam C++
+#include "iostream"
+
+// tidak disarankan menggunakan seperti ini
+// supaya menyingkat penulisan
+using namespace std;
+
+
+int main(){
+  
+  int bilangan;
+  
+  //mengawali layar untuk ncurses
+  initscr();
+  
+  // membersihkan layar
+  clear();
+  
+  // menampilkan tulisan di layar pada posisi 5,10
+  mvprintw(5,10,"Jurusan Ilmu Komputer");
+  
+  // memasukkan suatu nilai
+  mvprintw(7,10,"Masukan suatu bilangan: ");
+  scanw("%d",&bilangan);
+  
+  // menahan tampilan layar
+  getch();
+  
+  //mengakhiri layar ncurses
+  endwin();
+  
+  // hasil fungsi main() bertipe integer
+  return 11;
+}
+
+
+
+
+
+
