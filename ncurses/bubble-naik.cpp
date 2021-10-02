b65f3364234dead2eac070047e247858dfeb4605
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-10-02 11:53:08 +0700

    Update bubble-naik.cpp

--- ncurses/bubble-naik.cpp
+++ ncurses/bubble-naik.cpp
@@ -1,96 +1,89 @@
+/* ---------------------------------------------------
+ * program bubble short menggunakan bahasa C++
+ * dibuat oleh dwi sakethi
+ * 9 desember 2020
+ * program ini digunakan untuk mengurutkan data array
+ * dari angka kecil ke angka besar
+ * proses ngodingnya dapat disimak di youtube
+ * program pengurutan ini pada awalnya dibuat pada
+ * versi C, kemudian dikembangkan ke C++
+ * dan pada 2 oktober 2022 dikembangkan lagi ke
+ * versi C++ dan ncurses
+ * perbedaan hanya pada penulisan input dan output
+ * ---------------------------------------------------
+ * */
+ 
+// menentukan jumlah data maksimum
+#define jumlah_data 7
 
-/* -------------------------------------------------------------
-   contoh ini telah di-compile pada sistem operasi Windows 
-   dengan compiler gcc. nama file buble.cpp
-   perintah compile : gcc urut-naik.cpp -o urut -lm        
-   jika tidak ada kesalahan, program dapat dijalankan dengan 
-   memberikan perintah:
-
-   ./urut
-
-   #Muhammad Arief Islamy 3 Desember 2020
-   #Dwi Sakethi
-
-   program ini adalah alternatif bagaimana melakukan
-   pengurutan data dengan array   
-   ------------------------------------------------------------- */
-
-// menyertakan library untuk mencetak data ke layar
+// karena ada cout, maka perlu ini
 #include <iostream>
+
+// ini sebenarnya tidak dianjurkan
 using namespace std;
 
-// misalkan banyaknya data maksimal yang akan diolah ada 100 data
-#define jumlah_data_maksimum 100
-
+// program bahasa C++ paling tidak terdiri dari
+// satu fungsi, yaitu fungsi main()
 int main()
+{	
+// deklarasi variabel yang digunakan di dalam program
+int baris,kanan;
+int angka_kiri,angka_kanan;
+float nilai_sementara;
+float nilai[jumlah_data];
+
+// data ditulis di sini, supaya tidak capai memasukkan data
+// melalui keyboard
+nilai[0] = 7.5;
+nilai[1] = 3.2;
+nilai[2] = 5.5;
+nilai[3] = 9.5;
+nilai[4] = 3.1;
+nilai[5] = 8.0;
+nilai[6] = 6.5;
+
+/* --------------------------------------------------
+ * menampilkan data awal
+ * -------------------------------------------------- */
+
+cout << "Sebelum di-sorting " <<endl;
+
+for(baris=0;baris<jumlah_data;baris++)
 {
-   int banyak_data;
-   int data[jumlah_data_maksimum];
-   int data_ke,data_setelah,bilangan_sementara;
-   
-   // menghapus layar
-   // system("clear");
+  cout << "Nilai : "<< nilai[baris]<<endl;
+}
 
-   // menanyakan banyaknya data
-   cout<<"Banyaknya data: "; 
-   cin>>banyak_data;
-
-   // looping pemasukan data
-   for (data_ke=0;data_ke<banyak_data;data_ke++)
-       {
-        // memasukkan data ke-?
-        cout<<"Masukkan data ke-"<<data_ke+1<<" : ";
-        cin>>data[data_ke];
-
-       }
-   
-/* pengurutan data dengan metode buble sort */
-
-/* -------------------------------------------------
-   pembandingan dilakukan dari data pertama 
-   sampai dengan data terakhir-1 
-   karena perbandingan terakhir adalah perbandingan
-   antara data terakhir-1 dengan data terakhir
-   0-1 0-2 0-3 0-4 
-       1-2 1-3 1-4
-           2-3 2-4
-               3-4
-   terlihat bahwa nilai yang sebelah kiri, 
-   nilai terbesarnya adalah 3 (untuk contoh 5 data)
-   sehingga batas loopingnya banyak_data-2 
-   karena array dimulai dari angka 0,
-   maka akhir looping harus dikurangi lagi
-   ------------------------------------------------- */
-
-/* -------------------------------------------------
-   langkah pertama adalah membuat looping dengan 
-   hasil angka seperti berikut: 
-   0-1 0-2 0-3 0-4 
-       1-2 1-3 1-4
-           2-3 2-4
-               3-4
-   ------------------------------------------------- */
-
-  for (data_ke=0;data_ke<=banyak_data-2;data_ke++)
-   for (data_setelah=data_ke+1;
-        data_setelah<=banyak_data-1;data_setelah++)
-
-/* mengurutkan data dari kecil ke besar */
-   if (data[data_setelah]<data[data_ke])
-
-/* ditukar tempatnya, misal data[0] = 11 dan data[1]=6
-   maka ditukar sehingga menjadi data[0]=6 data[1]=11  */
+// proses bubble short dari kecil ke besar
+for (baris=1;baris<jumlah_data;baris++)
+{
+  kanan = jumlah_data+1-baris;
+  cout << baris << " - " << kanan << " : ";
+  for (angka_kiri=1;angka_kiri<kanan;angka_kiri++)
+  {
+    angka_kanan = angka_kiri+1;
+    cout << angka_kiri << " - " << angka_kanan << "  ";
+	
+	/* -------------------------------------------------
+	   jika data di sebelah kiri lebih besar dari data
+	   di sebelah kanan, maka posisinya ditukar
+	   ------------------------------------------------- */
+    if (nilai[angka_kiri-1]>nilai[angka_kanan-1])
       {
-         bilangan_sementara = data[data_ke];
-         data[data_ke]      = data[data_setelah];
-         data[data_setelah] = bilangan_sementara;
+         nilai_sementara      = nilai[angka_kiri-1];
+	     nilai[angka_kiri-1]  = nilai[angka_kanan-1];
+	     nilai[angka_kanan-1] = nilai_sementara;
       }
+  }
+  cout << endl;
+}
 
-/* cetak data hasil sorting */
-   cout<<"Data hasil sorting dengan Bubble Short: \n";   
-   for (data_ke=0;data_ke<=banyak_data-1;data_ke++)
-       cout<<" "<<data_ke+1<<" : "<<data[data_ke]<<endl;
+cout << "Setelah di-sorting " <<endl;
+for(baris=0;baris<jumlah_data;baris++)
+{
+  cout << "Nilai : "<< nilai[baris]<<endl;
+}
 
-  // karena int main, maka harus ada return
-  return 0;   
+// akhir program
+// karena int main(), jadi boleh diisi dengan 1
+return 1;
 }
