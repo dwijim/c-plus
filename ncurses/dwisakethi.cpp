53dbff762a0200ab1ddf24ae11fd471bd84704d8
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2021-10-11 17:02:05 +0700

    Update dwisakethi.cpp

--- ncurses/dwisakethi.cpp
+++ ncurses/dwisakethi.cpp
@@ -49,7 +49,15 @@ int main ()
   
 // mendefinisikan kalimat string 60 karakter
 // diberi spasi fungsinya untuk menghapus	
- string kalimat=" dwi sakethi ilmu komputer unila ";
+ string kalimat01="  wi sakethi ilmu komputer unila ";
+ string kalimat02="  wi sakethi ilmu komputer unila ";
+ string kalimat03="  wi sakethi ilmu komputer unila ";
+ string kalimat04="  wi sakethi ilmu komputer unila ";
+ string kalimat05=" dwi sakethi ilmu komputer unila ";
+ string kalimat06=" dwi sakethi ilmu komputer unila ";
+ string kalimat07=" dwi sakethi ilmu komputer unila ";
+ string kalimat08=" dwi sakethi ilmu komputer unila ";
+ string kalimat09=" dwi sakethi ilmu komputer unila ";
 //char kalimat[]=" dwi sakethi ";
 
 // kiri untuk tulisan sebelah kiri
