ab464a43451c1eb288f21bd4f5361cb40ba1160b
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2022-03-21 08:02:10 +0700

    Create looptanpahenti.cpp

---
+++ looptanpahenti.cpp
@@ -0,0 +1,39 @@
+/* ---------------------------------------------
+ * program untuk menunjukkan salah satu
+ * contoh looping yang tidak berhenti
+ * dibuat oleh dwi sakethi:
+ * - 21 maret 2022
+ * 
+ * 
+ * file ini disimpan dengan nama looptanpahenti.cpp
+ * compile g++ looptanpahenti.cpp -o coba 
+ * 
+ * dijalankan dengan perintah: ./coba
+ * --------------------------------------------- */
+
+
+// karena ada fungsi input dan output
+#include <iostream>
+
+// untuk menyingkat penulisan tapi sebenarnya ...
+using namespace std;
+
+// program dalam bahasa C++ harus ada fungsi main()
+int main()
+{
+ 
+   // deklarasi variabel
+   long int baris;
+   
+  // kesalahan logika sehingga looping berjalan terus
+    while (1>0)
+     {
+        // mencetak pada baris yang bertambah
+        // supaya tulisan tambah ke bawah
+        cout << baris << "\n";
+        baris++;
+     }    
+    
+   // kembali ke program utama
+   return 0;
+}
