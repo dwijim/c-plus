7abcc9a3e123e8c3eb402da316d3d27c713737d4
dwi sakethi <44912078+dwijim@users.noreply.github.com>
2026-09-21 09:46:40 +0700

    Update ilkom.cpp to fix variable declaration
    Fix variable declaration and improve comments.

--- ilkom.cpp
+++ ilkom.cpp
@@ -1,23 +1,40 @@
+/* ---------------------------------------
+ * program untuk mencetak suatu tulisan
+ * sebanyak n kali
+ * dwi sakethi, 21 september 2026
+ * nama file : ilkom.cpp
+ * compile : g++ ilkom.cpp -o ulang
+ * --------------------------------------- */
+
 #include "iostream"
-/* #include "string" */
+// karena ada perintah cin dan cout
 
 using namespace std;
+// untuk mempermudah penulisan perintah
 
+
+// program dalam bahasa c++ minimal terdiri dari
+// satu fungsi yaitu fungsi main()
 int main()
 {
+    deklarasi variabel yang digunakan
 	short int baris, tulisan_ke;
-	string tulisan;
 
+   // menanyakan tulisan yang akan dicetak
    cout << "Tulisan yang akan dicetak : ";
    cin >> tulisan;
 
+   // menanyakan akan dicetak berapa kali
    cout << "Berapa baris : ";
    cin >> baris;
 
+   // looping dengan for karena sudah jelas jumlah looping-nya
    for (tulisan_ke=1;tulisan_ke<=baris;tulisan_ke++)
    {
       cout << tulisan << "\n";
+      // mencetak tulisan
    }
 
    return 37;
+   // akhir program menghasilan bilang int
 }
